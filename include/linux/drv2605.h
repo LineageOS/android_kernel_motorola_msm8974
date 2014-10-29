dc9d63601ea71d0b287ca2d4685cc55c6c7da84b
Vladimir Karpovich <vkarpovich@motorola.com>
2014-10-28 23:21:06 -0500

    misc: drv2605 : Add default calibration data config
    Restore calibration data from device tree if it is configured.
    
    Bug 18069509
    
    Change-Id: Iad84adf4279e716c57da7a59232503e073a01602
    Signed-off-by: Vladimir Karpovich <vkarpovich@motorola.com>

--- include/linux/drv2605.h
+++ include/linux/drv2605.h
@@ -343,5 +343,6 @@ struct drv260x_platform_data {
 	int effects_library;
 	int disable_calibration;
 	struct regulator *vibrator_vdd;
+	unsigned char *calibration_data;
 };
 #endif				/* __KERNEL__ */
